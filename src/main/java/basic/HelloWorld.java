dfb151d78163490d2ef38ed2dcfc1f01ea0c350b
imasy2k <imasy2k@hotmail.com>
2025-06-10 01:31:58 +0100

    added code in b1 branch

--- src/main/java/basic/HelloWorld.java
+++ src/main/java/basic/HelloWorld.java
@@ -5,7 +5,13 @@ public class HelloWorld {
 	public static void main(String[] args) {
 	
 		System.out.println("Hello there!! welcome tp the devops training");
-
+		
+		
+		
+	}
+	public void m1()
+	{
+		System.out.println(" form m1");
 	}
 
 }
